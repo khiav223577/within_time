24a3cfb44e2bee1a8ed59c727031a85482edeeac
khiav223577 <mrtmrt15xn@yahoo.com.tw>
2021-05-04 15:18:44 +0800

    move logic out of lambda to enhance runtime performance

--- lib/within_time.rb
+++ lib/within_time.rb
@@ -4,7 +4,12 @@ require 'within_time/bsearch_service'
 module WithinTime
   class << self
     def build_scope(column:, has_index: false, klass: nil)
-      ->(time_range){ WithinTime::BSearchService.new(klass || self).bsearch_time_range(column, time_range) }
+      if klass
+        service = WithinTime::BSearchService.new(klass)
+        return ->(time_range){ service.bsearch_time_range(column, time_range) }
+      end
+
+      return ->(time_range){ WithinTime::BSearchService.new(self).bsearch_time_range(column, time_range) }
     end
   end
 end
