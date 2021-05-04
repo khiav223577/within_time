e40b5aafee07c9baa0073e100ac2e4686ec75242
khiav223577 <mrtmrt15xn@yahoo.com.tw>
2021-05-04 15:11:53 +0800

    cannot using klass instead of self in rails 6.1

--- test/lib/models/user.rb
+++ test/lib/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class User < ActiveRecord::Base
-  scope :created_within, WithinTime.build_scope(self, column: :created_at, has_index: true)
+  if Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('5.0.0')
+    scope :created_within, WithinTime.build_scope(column: :created_at, has_index: true)
+  else
+    scope :created_within, WithinTime.build_scope(column: :created_at, has_index: true, klass: self)
+  end
 end
